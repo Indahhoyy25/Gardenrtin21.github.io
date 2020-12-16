5bff4a4673915b7657bd4d91202176ee7ef1eecd
Gardenrtin21 <76092018+Gardenrtin21@users.noreply.github.com>
2020-12-16 16:49:03 +0700

    Add files via upload

---
+++ mm.js
@@ -0,0 +1,7 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Freelancer", "Random People", "College Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
---
+++ mm2.js
@@ -0,0 +1,14 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Freelancer", "Random People", "College Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing2", {
+        strings: ["Freelancer", "Random People", "College Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
