36fa281cbf585ba796bb370572ff0722831d72a5
Indahhoyy25 <118248085+Indahhoyy25@users.noreply.github.com>
2022-11-27 16:04:17 +0700

    Update mm.js
    Signed-off-by: Indahhoyy25 <118248085+Indahhoyy25@users.noreply.github.com>

--- mm.js
+++ mm.js
@@ -1,6 +1,6 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Freelancer", "Random People", "College Student"],
+        strings: ["Freelancer", "Librarian", "College Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
